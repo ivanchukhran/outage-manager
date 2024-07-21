f47b7242d6690effa73d6403dd96a8312f29590a
Ivan Chukhran <ivan.chuhran@gmail.com>
2024-07-21 10:39:20 +0300

    try user-agents

--- outage-manager/parser.py
+++ outage-manager/parser.py
@@ -41,11 +41,20 @@ class OutageStatus(Enum):
     ACTIVE = "ACTIVE"
     INACTIVE = "INACTIVE"
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+}
+
+
 async def get_page_content(url: str = URL) -> str:
     async with async_playwright() as p:
         chromium = p.chromium
-        browser = await chromium.launch()
+        browser = await chromium.launch(headless=True)
         page = await browser.new_page()
+        await page.set_extra_http_headers(headers)
         await page.goto(url)
         response = await page.content()
         await browser.close()
